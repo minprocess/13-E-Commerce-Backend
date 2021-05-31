23876954040b429415432b1fbbbbf062d24489c1
Bill Pate <wtpate69@hotmail.com>
2021-05-31 16:22:13 -0400

    change back to process.env.JAWSDB_GOLD_URL

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+console.log("JAWSDB_GOLD_URL");
+console.log(JAWSDB_GOLD_URL);
+
+
 // ?: trigraph operator 
 // if process.env.JAWSDB_URL is true then use us JawsDB, if not use mySQL
-const sequelize = process.env.JAWSDB_GOLD
-  ? new Sequelize(process.env.JAWSDB_GOLD)
+const sequelize = process.env.JAWSDB_GOLD_URL
+  ? new Sequelize(process.env.JAWSDB_GOLD_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
